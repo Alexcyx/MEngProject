07940a769d8a95a6e81b294a036380efbff41949
githubloisxin <zhouxylois@gmail.com>
2019-03-10 13:46:17 -0400

    fromXinyue_1

---
+++ src/pages/Dashboard/PersonInfoPage.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="content">
+  <div class="md-layout">
+    <person-info></person-info>
+  </div>
+  </div>
+</template>
+
+<script>
+import PersonInfo from "@/components/MyComponents/PersonInfo.vue";
+
+export default {
+  components: {
+    'person-info': PersonInfo
+  }
+  
+};
+</script>
